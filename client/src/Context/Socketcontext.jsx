7efbeb5ea8720d525bffffbfaa043771eed08b94
Lokeshwar <116664452+lokesh000000@users.noreply.github.com>
2024-08-08 01:11:04 +0530

    Update Socketcontext.jsx

--- client/src/Context/Socketcontext.jsx
+++ client/src/Context/Socketcontext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:4000", {
+            const socket = io("https://chatapp-g71t.onrender.com", {
                 query: {
                     userId: authUser.id,
                 },
